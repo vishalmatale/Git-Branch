6090f52b2adbc3e9845759287f7e4ec26c5b72cc
vishalmatale <you@example.com>
2022-08-13 16:13:16 +0530

    model layer

---
+++ src/main/java/com/tcs/GitBranchApplication.java
@@ -0,0 +1,13 @@
+package com.tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBranchApplication.class, args);
+	}
+
+}
